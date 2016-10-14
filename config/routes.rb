25570a9adb843eccde9bbb1d574e6891fd61ca42
Sera Bak <serabak15@gmail.com>
2016-10-14 13:46:22 -0700

    creates root path and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'photos/index'
-
-  get 'photos/show'
-
-  get 'photos/create'
-
-  get 'photos/update'
-
-  get 'photos/destroy'
+ 
+	get '/', to: 'photos#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
