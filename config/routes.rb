9d24d1aabde6938fdf913d5833ab1aca5f8b936d
Sera Bak <serabak15@gmail.com>
2016-10-14 12:06:23 -0700

    creates basic app and photos controller

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'photos/index'
+
+  get 'photos/show'
+
+  get 'photos/create'
+
+  get 'photos/update'
+
+  get 'photos/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
