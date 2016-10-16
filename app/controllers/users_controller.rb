b4d75b1244faf8f4712bfe02ccd2575db5e0cd5b
ashleyvernon <avernon526@gmail.com>
2016-10-16 16:12:19 -0700

    adds forms for login to check if working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.create(user_params)
+		@user = User.new(user_params)
 		# login(@user)
 		# redirect_to @user
 		if @user.save
